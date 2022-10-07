609eced517cf99f8b264508e2782118bdad4f928
Nodirbek Dedamirzaev <112013952+DedamirzaevNodirbek@users.noreply.github.com>
2022-10-07 17:39:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const dayEl = document.getElementById("days");
+const monthEl = document.getElementById("month");
+const hourEl = document.getElementById("hour");
+const minutesEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+function updateClock() {
+  let d = new Date().getDay();
+  let month = new Date().getMonth();
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+  d = d < 10 ? "0" + d : d;
+  month = month < 10 ? "0" + month : month;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+  dayEl.innerText = d;
+  monthEl.innerText = month;
+  hourEl.innerText = h;
+  minutesEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl, (innerText = ampm);
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+updateClock();
